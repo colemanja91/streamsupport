00dbfbcec7e71a5658797df4dc3984a259c5473d
colemanja91 <colemanja91@gmail.com>
2024-07-02 19:05:20 -0400

    rename since this will be more than just an overlay

--- haloinfinite/apps.py
+++ haloinfinite/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OverlayConfig(AppConfig):
+class HaloinfiniteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'overlay'
+    name = 'haloinfinite'
